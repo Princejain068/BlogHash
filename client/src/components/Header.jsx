d371a5af4ed65304e4f1c842c248cbafcfa7f257
prince jain <princejain068@gmail.com>
2024-04-02 01:27:01 +0530

    delete node_modules

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -34,7 +34,7 @@ export default function Header() {
             </Button>
 
             <Link to='/sign-in'>
-                <Button gradientDuoTone='purpleToBlue' >Sign In
+                <Button gradientDuoTone='purpleToBlue' outline>Sign In
                 </Button>
             </Link>
 
